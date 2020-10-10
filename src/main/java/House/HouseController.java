cb67e04d9c4c36146fad2d63788ec2fe0fd03686
Matthew Kevork <matthew.kevork@hotmail.com>
2020-10-10 19:50:58 -0400

    Refactor findAll routes of all controllers to handle simple "equals" query params

--- src/main/java/House/HouseController.java
+++ src/main/java/House/HouseController.java
@@ -11,6 +11,7 @@ import io.javalin.apibuilder.CrudHandler;
 import io.javalin.http.Context;
 import Data.MongoDBConnection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,11 +19,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.function.Consumer;
 
+import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 
 /**
@@ -45,14 +48,14 @@ public class HouseController implements CrudHandler {
      */
     public static void uploadHouseLayoutFile(@NotNull Context context) throws IOException {
         //extract json content from house layout file
-        String jsonHouse = IOUtils.toString(context.uploadedFile("house_layout").getContent(), StandardCharsets.UTF_8);
+        String jsonHouseLayout = IOUtils.toString(context.uploadedFile("house_layout").getContent(), StandardCharsets.UTF_8);
 
         //parse json into java object HouseLayout
         Gson gson = new Gson();
-        HouseLayout houseLayout = gson.fromJson(jsonHouse, HouseLayout.class);
+        HouseLayout houseLayout = gson.fromJson(jsonHouseLayout, HouseLayout.class);
 
         //insert new House into collection
-        House house = new House(new ObjectId(), context.formParam("house_name"));
+        House house = new House(new ObjectId(), context.formParam("house_name"), new ObjectId(context.pathParam("user-id")));
         houseCollection.insertOne(house);
 
         //iterate over rooms in layout file and insert into database as Room objects
@@ -70,11 +73,41 @@ public class HouseController implements CrudHandler {
      */
     public void getAll(@NotNull Context context) {
         LOGGER.info("Get all Houses");
-        ArrayList<House> houseslist = new ArrayList<>();
-        FindIterable<House> houses = houseCollection.find();
-        houses.forEach((Consumer<House>) houseslist::add);
-        System.out.println(houseslist);
-        context.json(houseslist);
+
+
+        ArrayList<Bson> filters = new ArrayList<>();
+
+        //iterate over fields of House class
+        for (Field f : House.class.getDeclaredFields()) {
+            String fieldName = f.getName();
+            Class fieldType = f.getType();
+
+            //check if the field is in query params
+            if (context.queryParam(fieldName) != null) {
+
+                //check if the field is a boolean, int, ObjectId or other
+                if (fieldType.equals(boolean.class)) {
+                    filters.add(eq(fieldName, Boolean.parseBoolean(context.queryParam(fieldName))));
+                } else if (fieldType.equals(int.class)) {
+                    filters.add(eq(fieldName, Integer.parseInt(context.queryParam(fieldName))));
+                } else if (fieldType.equals(ObjectId.class)) {
+                    filters.add(eq(fieldName, new ObjectId(context.queryParam(fieldName))));
+                } else {
+                    filters.add(eq(fieldName, context.queryParam(fieldName)));
+                }
+            }
+        }
+
+        //join query param filters with logical ANDs
+        Bson filter = and(filters);
+
+        //query database with filter
+        FindIterable<House> houses = houseCollection.find(filter);
+
+        //construct arrayList out with query results and send as json response
+        ArrayList<House> houseList = new ArrayList<>();
+        houses.forEach((Consumer<House>) houseList::add);
+        context.json(houseList);
     }
 
     /**
