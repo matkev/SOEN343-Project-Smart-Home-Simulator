99190d546669d5f98d8f26801f3d02e0829346f1
Matthew Kevork <matthew.kevork@hotmail.com>
2020-10-21 15:28:33 -0400

    Refactor App.js routes and login logic

--- src/dashboard/src/App.js
+++ src/dashboard/src/App.js
@@ -6,21 +6,24 @@ import Layout from "./Components/Layout/Layout";
 import DashboardPage from "./Pages/Dashboard/DashboardPage";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import ManageHouse from "./Pages/ManageHouse/ManageHouse";
-import ManageAgents from "./Pages/ManageUsers/ManageAgents";
+import ManageUsers from "./Pages/ManageUsers/ManageUsers";
+import ManageHouses from "./Pages/ManageHouses/ManageHouses";
+import ManageHouseLayout from "./Pages/ManageHouseLayout/ManageHouseLayout";
+import ManageAgents from "./Pages/ManageAgents/ManageAgents";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <PublicRoute path="/login" component={AuthPage}/>
+          <PublicRoute path="/login" component={ManageUsers}/>
           <PrivateRoute path={"/"} render={() =>
             <Layout>
               <Switch>
                 <Route exact path={"/"} component={DashboardPage}/>
+                <Route path={"/manage-houses"} component={ManageHouses}/>
                 <Route path={"/manage-agents"} component={ManageAgents}/>
-                <Route path={"/manage-house"} component={ManageHouse}/>
+                <Route path={"/manage-house-layout"} component={ManageHouseLayout}/>
                 <Route component={DashboardPage}/>
               </Switch>
             </Layout>
@@ -33,23 +36,25 @@ const App = () => {
 };
 
 
-const isLogin = () => !!localStorage.getItem("x-auth-token");
+// const isLogin = () => !!localStorage.getItem("userId");
 
 const PublicRoute = ({component, ...props}) => {
   return <Route {...props} render={(props) => {
-    if (isLogin())
-      return <Redirect to={"/"}/>
-    else {
-      return React.createElement(component, props);
-    }
+    // if (isLogin())
+    //   return <Redirect to={"/"}/>
+    // else {
+    //   return React.createElement(component, props);
+    // }
+    return React.createElement(component, props);
   }}/>
 };
 
 const PrivateRoute = ({render, ...props}) => {
   return <Route {...props} render={(props) => {
-    if (isLogin())
-      return render(props);
-    else return <Redirect to={"/login"}/>
+    // if (isLogin())
+    //   return render(props);
+    // else return <Redirect to={"/login"}/>
+    return render(props);
   }}/>
 }
 
